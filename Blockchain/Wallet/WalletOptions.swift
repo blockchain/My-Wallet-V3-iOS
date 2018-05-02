af69396f21b2d4cd013e4d731b5572b047e3786e
Maurice Achtenhagen <maurice@blockchain.com>
2018-05-02 17:20:12 -0400

    feat(Wallet Options): add source file

---
+++ Blockchain/Wallet/WalletOptions.swift
@@ -0,0 +1,14 @@
+//
+//  WalletOptions.swift
+//  Blockchain
+//
+//  Created by Maurice A. on 5/1/18.
+//  Copyright © 2018 Blockchain Luxembourg S.A. All rights reserved.
+//
+
+import Foundation
+
+enum WalletOptions: String {
+    case maintenance = "maintenance"
+    case mobileInfo = "mobileInfo"
+}
