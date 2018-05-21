efb29ca688ba57a64a60e88cfd77c2de5a5f31ca
woudini <10579422+woudini@users.noreply.github.com>
2018-05-21 09:55:46 -0400

    refactor(RootService): set app coordinator as wallet history delegate

---
+++ Blockchain/Wallet/WalletHistoryDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  WalletHistoryDelegate.swift
+//  Blockchain
+//
+//  Created by kevinwu on 5/21/18.
+//  Copyright © 2018 Blockchain Luxembourg S.A. All rights reserved.
+//
+
+import Foundation
+
+@objc protocol WalletHistoryDelegate: class {
+
+    /// Method invoked when getting transaction history fails
+    func didFailGetHistory(error: String?)
+}
