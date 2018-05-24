7faa55a9bce3b6f85fae804d238c76ce4b2fd0f1
Chris Arriola <arriola@blockchain.com>
2018-05-24 08:59:44 -0700

    fix(bitcoinurl): support web format.

--- Blockchain/URL/BitcoinURLPayload.swift
+++ Blockchain/URL/BitcoinURLPayload.swift
@@ -33,11 +33,28 @@ extension BitcoinURLPayload {
             return nil
         }
 
+<<<<<<< Updated upstream
         let queryArgs = url.queryArgs
 
         let address = url.host ?? queryArgs["address"]
         let amount = queryArgs["amount"]
 
         self.init(address: address, amount: amount)
+=======
+        let urlString = url.absoluteString
+        if urlString.contains("//") {
+            let queryArgs = url.queryArgs
+
+            self.address = url.host ?? queryArgs["address"]
+            self.amount = queryArgs["amount"]
+        } else if let commaIndex = urlString.index(of: ":") {
+            // Handle web format (e.g. "bitcoin:1Amu4uPJnYbUXX2HhDFMNq7tSneDwWYDyv")
+            self.address = String(urlString[urlString.index(after: commaIndex)..<urlString.endIndex])
+            self.amount = nil
+        } else {
+            self.address = nil
+            self.amount = nil
+        }
+>>>>>>> Stashed changes
     }
 }
