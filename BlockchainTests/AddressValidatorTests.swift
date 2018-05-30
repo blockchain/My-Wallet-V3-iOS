4476355bf3332631678a5aac668a0756bedc6ffd
Maurice Achtenhagen <maurice@blockchain.com>
2018-05-30 16:04:09 -0400

    feat(Address Validation): update unit tests

--- BlockchainTests/AddressValidatorTests.swift
+++ BlockchainTests/AddressValidatorTests.swift
@@ -10,51 +10,90 @@ import XCTest
 
 class AddressValidatorTests: XCTestCase {
 
+    var addressValidator: AddressValidator?
+
     override func setUp() {
         super.setUp()
         WalletManager.shared.wallet.loadJS()
         precondition((WalletManager.shared.wallet.context != nil), "JS context is required for use of AddressValidator")
+        addressValidator = AddressValidator.shared
     }
 
     override func tearDown() {
         super.tearDown()
     }
 
-    // MARK: - P2PKH Addresses
-
-    func testBitcoinAddressValidatorWithValidP2PKHAddress() {
-        let address = BitcoinAddress(string: "1W3hBBAnECvpmpFXcBrWoBXXihJAEkTmA")
-        XCTAssertTrue(AddressValidator.shared!.validate(bitcoinAddress: address), "Expected address to be valid.")
+    func testAddressValidatorInitializer() {
+        XCTAssertNotNil(addressValidator, "Expected the address validator to have initialized with the JS context.")
     }
 
-    func testBitcoinAddressValidatorWithInValidP2PKHAddress() {
+    // MARK: - P2PKH Addresses
+
+    func testAddressValidatorWithValidP2PKHAddress() {
+        precondition(addressValidator != nil, "Address validator must not be nil!")
+        let address = BitcoinAddress(string: "1W3hBBAnECvpmpFXcBrWoBXXihJAEkTmA")
+        XCTAssertTrue(addressValidator!.validate(bitcoinAddress: address), "Expected address to be valid.")
+    }
+
+    func testAddressValidatorWithInValidP2PKHAddress() {
+        precondition(addressValidator != nil, "Address validator must not be nil!")
         let address = BitcoinAddress(string: "1W3hBBAnECvpmpFXcBrWoBXXihJAEkTmO")
-        XCTAssertFalse(AddressValidator.shared!.validate(bitcoinAddress: address), "Expected address to be invalid.")
+        XCTAssertFalse(addressValidator!.validate(bitcoinAddress: address), "Expected address to be invalid.")
     }
 
     // MARK: - P2SH Addresses (Multi-sig)
 
-    func testBitcoinAddressValidatorWithValidP2SHAddress() {
+    func testAddressValidatorWithValidP2SHAddress() {
+        precondition(addressValidator != nil, "Address validator must not be nil!")
         let address = BitcoinAddress(string: "3J98t1WpEZ73CNmQviecrnyiWrnqRhWNLy")
-        XCTAssertTrue(AddressValidator.shared!.validate(bitcoinAddress: address), "Expected address to be valid.")
+        XCTAssertTrue(addressValidator!.validate(bitcoinAddress: address), "Expected address to be valid.")
     }
 
-    // MARK: - Invalid Addresses
+    // MARK: - Bitcoin Address Validation
 
-    func testBitcoinAddressValidatorWithShortAddress() {
+    func testAddressValidatorWithShortBitcoinAddress() {
+        precondition(addressValidator != nil, "Address validator must not be nil!")
         let address = BitcoinAddress(string: "abc")
-        XCTAssertFalse(AddressValidator.shared!.validate(bitcoinAddress: address), "Expected address to be invalid.")
+        XCTAssertFalse(addressValidator!.validate(bitcoinAddress: address), "Expected address to be invalid.")
     }
 
-    func testBitcoinAddressValidatorWithLongAddress() {
+    func testAddressValidatorWithLongBitcoinAddress() {
+        precondition(addressValidator != nil, "Address validator must not be nil!")
         let address = BitcoinAddress(string: "ThisBitcoinAddressIsWayTooLongToBeValid")
-        XCTAssertFalse(AddressValidator.shared!.validate(bitcoinAddress: address), "Expected address to be invalid.")
+        XCTAssertFalse(addressValidator!.validate(bitcoinAddress: address), "Expected address to be invalid.")
     }
 
-    func testBitcoinAddressValidatorWithEmptyAddress() {
+    func testAddressValidatorWithEmptyAddress() {
+        precondition(addressValidator != nil, "Address validator must not be nil!")
         let address = BitcoinAddress(string: "")
-        XCTAssertFalse(AddressValidator.shared!.validate(bitcoinAddress: address), "Expected address to be invalid.")
+        XCTAssertFalse(addressValidator!.validate(bitcoinAddress: address), "Expected address to be invalid.")
     }
 
-    // TODO: add tests for validating BCH and ETH addresses
+    // MARK: - Bitcoin Cash Address Validation
+
+    func testAddressValidatorWithValidBitcoinCashAddress() {
+        precondition(addressValidator != nil, "Address validator must not be nil!")
+        let address = BitcoinCashAddress(string: "qz2js9054gqxj4dww35kkc3jpf0ph4cfh53tld3zek")
+        XCTAssertTrue(addressValidator!.validate(bitcoinCashAddress: address), "Expected address to be invalid.")
+    }
+
+    func testAddressValidatorWithInvalidBitcoinCashAddress() {
+        precondition(addressValidator != nil, "Address validator must not be nil!")
+        let address = BitcoinCashAddress(string: "DoNotSendYourMoneyToThisAddress")
+        XCTAssertFalse(addressValidator!.validate(bitcoinCashAddress: address), "Expected address to be invalid.")
+    }
+
+    // MARK: - Ethereum Address Validation
+
+    func testAddressValidatorWithValidEthereumAddress() {
+        precondition(addressValidator != nil, "Address validator must not be nil!")
+        let address = EthereumAddress(string: "0x4A17aE59898427E2c7fE4eB6Cdd88D357F76AA99")
+        XCTAssertTrue(addressValidator!.validate(ethereumAddress: address), "Expected address to be invalid.")
+    }
+
+    func testAddressValidatorWithInvalidEthereumAddress() {
+        precondition(addressValidator != nil, "Address validator must not be nil!")
+        let address = EthereumAddress(string: "DoNotSendYourEtherToThisAddress")
+        XCTAssertFalse(addressValidator!.validate(ethereumAddress: address), "Expected address to be invalid.")
+    }
 }
