c82c8503b1d7709ef2791ca29ed71eb305e095be
Chris Arriola <arriola@blockchain.com>
2018-06-19 17:16:52 -0700

    fix(tx_detail): make sure share for an ether tx directs to etherscan.

---
+++ Blockchain/AssetTypeLegacyHelper.swift
@@ -0,0 +1,24 @@
+//
+//  AssetTypeLegacyHelper.swift
+//  Blockchain
+//
+//  Created by Chris Arriola on 6/19/18.
+//  Copyright © 2018 Blockchain Luxembourg S.A. All rights reserved.
+//
+
+import Foundation
+
+/// Helper to convert between AssetType <-> LegacyAssetType.
+// To be deprecated once LegacyAssetType has been removed.
+@objc class AssetTypeLegacyHelper: NSObject {
+    @objc static func convert(fromLegacy type: LegacyAssetType) -> AssetType {
+        switch type {
+        case .bitcoin:
+            return .bitcoin
+        case .ether:
+            return .ethereum
+        case .bitcoinCash:
+            return .bitcoinCash
+        }
+    }
+}
