7aed865e0f0b8b104e83a2aa72f98c7c27111624
Justin <justinsl@gmail.com>
2018-08-08 14:54:01 -0400

    feat(KYC): Add Onfido

--- Blockchain/KYC/KYCVerifyIdentityController.swift
+++ Blockchain/KYC/KYCVerifyIdentityController.swift
@@ -63,8 +63,8 @@ final class KYCVerifyIdentityController: UIViewController {
     private func onfidoConfigurator(_ document: DocumentType, countryCode: String) -> OnfidoConfig {
         //swiftlint:disable next force_try
         let config = try! OnfidoConfig.builder()
-            .withToken("123345")
-            .withApplicantId("somebody once told me")
+            .withToken("")
+            .withApplicantId("")
             .withDocumentStep(ofType: document, andCountryCode: countryCode)
             .withFaceStep(ofVariant: .photo) // specify the face capture variant here
             .build()
