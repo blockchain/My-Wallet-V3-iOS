fe49d3f70b453f656a4b24bb0b8e7e001fcb923e
Alex McGregor <amcgregor@blockchain.com>
2018-08-27 17:51:05 -0500

    undoing debug code.

--- Blockchain/Coordinators/ExchangeCoordinator.swift
+++ Blockchain/Coordinators/ExchangeCoordinator.swift
@@ -55,7 +55,7 @@ struct ExchangeServices: ExchangeDependencies {
     func start() {
         if WalletManager.shared.wallet.hasEthAccount() {
             let success = { [weak self] (isHomebrewAvailable: Bool) in
-                if !isHomebrewAvailable {
+                if isHomebrewAvailable {
                     self?.showExchange(type: .homebrew)
                 } else {
                     self?.showExchange(type: .shapeshift)
