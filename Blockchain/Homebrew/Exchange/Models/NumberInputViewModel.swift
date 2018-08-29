0e752664401216f973266b82329cb3780cbde888
woudini <10579422+woudini@users.noreply.github.com>
2018-08-29 10:51:43 -0400

    test(Homebrew-Shapeshift): add tests for number input model

--- Blockchain/Homebrew/Exchange/Models/NumberInputViewModel.swift
+++ Blockchain/Homebrew/Exchange/Models/NumberInputViewModel.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 protocol NumberInputDelegate: class {
-    var input: String? { get }
+    var input: String { get }
     func add(character: String)
     func addDecimal()
     func backspace()
@@ -18,40 +18,72 @@ protocol NumberInputDelegate: class {
 // Class used to store the results of user input relayed by the NumberKeypadView.
 class NumberInputViewModel: NumberInputDelegate {
 
-    private(set) var input: String?
+    private let numbers: Set<String> = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    private let zero = "0"
+    private(set) var input: String
+
+    init() {
+        input = zero
+    }
 
     init(newInput: String?) {
-        input = newInput
+        input = zero
+        // TODO: Sanitize newInput
+        input = newInput ?? zero
     }
 
     func add(character: String) {
-        // If current input is nil, set to character
-        guard input != nil else {
+        guard numbers.contains(character) else {
+            Logger.shared.error("Invalid character")
+            return
+        }
+
+        guard character.count == 1 else {
+            Logger.shared.error("Character must be a single element")
+            return
+        }
+
+        // If current input is zero, set to character
+        guard input != zero else {
             input = character
             return
         }
-        input = input! + character
+        input += character
     }
 
     func addDecimal() {
-        guard let decimalSeparator = Locale.current.decimalSeparator else {
-            Logger.shared.warning("No decimal separator available")
-            return
-        }
-
-        guard input != nil else {
-            input = "0" + decimalSeparator
-            return
-        }
-
-        guard !input!.contains(decimalSeparator) else {
+        // Allow only one decimal separator
+        guard !input.contains(decimalSeparator) else {
             Logger.shared.debug("Decimal already exists")
             return
         }
+
+        // If current input is zero, make it a leading zero and add decimal separator
+        guard input != zero else {
+            input = zero + decimalSeparator
+            return
+        }
+
+        input += decimalSeparator
     }
 
     func backspace() {
-        guard input != nil else { return }
-        input = String(input!.dropLast())
+        guard input.count > 1 else {
+            input = zero
+            return
+        }
+        input = String(input.dropLast())
+    }
+}
+
+// MARK: Helpers
+extension NumberInputViewModel {
+    var decimalSeparator: String {
+        // Make sure a decimal separator exists
+        guard let decimalSeparator = Locale.current.decimalSeparator else {
+            Logger.shared.warning("No decimal separator available, using period")
+            return "."
+        }
+        return decimalSeparator
     }
 }
