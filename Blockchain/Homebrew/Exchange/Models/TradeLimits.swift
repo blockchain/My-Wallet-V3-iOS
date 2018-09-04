fae1afde377e88c16ab33b77a3ebc74cf414b821
Alex McGregor <amcgregor@blockchain.com>
2018-09-04 10:25:33 -0500

    PR feedback.

--- Blockchain/Homebrew/Exchange/Models/TradeLimits.swift
+++ Blockchain/Homebrew/Exchange/Models/TradeLimits.swift
@@ -10,9 +10,9 @@ import Foundation
 
 struct TradeLimits: Decodable {
     let currency: String
-    let minOrder: Double
-    let maxOrder: Double
-    let maxPossibleOrder: Double
+    let minOrder: Decimal
+    let maxOrder: Decimal
+    let maxPossibleOrder: Decimal
     let daily: Limit
     let weekly: Limit
     let annual: Limit
@@ -30,18 +30,18 @@ struct TradeLimits: Decodable {
     init(from decoder: Decoder) throws {
         let values = try decoder.container(keyedBy: CodingKeys.self)
         currency = try values.decode(String.self, forKey: .currency)
-        minOrder = try values.decode(String.self, forKey: .minOrder).toDouble()
-        maxOrder = try values.decode(String.self, forKey: .maxOrder).toDouble()
-        maxPossibleOrder = try values.decode(String.self, forKey: .maxPossibleOrder).toDouble()
+        minOrder = try values.decode(String.self, forKey: .minOrder).toDecimal()
+        maxOrder = try values.decode(String.self, forKey: .maxOrder).toDecimal()
+        maxPossibleOrder = try values.decode(String.self, forKey: .maxPossibleOrder).toDecimal()
         daily = try values.decode(Limit.self, forKey: .daily)
         weekly = try values.decode(Limit.self, forKey: .weekly)
         annual = try values.decode(Limit.self, forKey: .annual)
     }
     
     struct Limit: Decodable {
-        let limit: Double
-        let available: Double
-        let used: Double
+        let limit: Decimal
+        let available: Decimal
+        let used: Decimal
         
         enum CodingKeys: String, CodingKey {
             case limit
@@ -51,9 +51,9 @@ struct TradeLimits: Decodable {
         
         init(from decoder: Decoder) throws {
             let values = try decoder.container(keyedBy: CodingKeys.self)
-            limit = try values.decode(String.self, forKey: .limit).toDouble()
-            available = try values.decode(String.self, forKey: .available).toDouble()
-            used = try values.decode(String.self, forKey: .used).toDouble()
+            limit = try values.decode(String.self, forKey: .limit).toDecimal()
+            available = try values.decode(String.self, forKey: .available).toDecimal()
+            used = try values.decode(String.self, forKey: .used).toDecimal()
         }
     }
 }
@@ -64,8 +64,8 @@ extension String {
         case generic
     }
     
-    func toDouble() throws -> Double {
-        guard let result = Double(self) else {
+    func toDecimal() throws -> Decimal {
+        guard let result = Decimal(string: self) else {
             throw ConversionError.generic
         }
         return result
