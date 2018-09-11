6771544be760f7d07d1cae49509f9088cc6b1670
Maurice Achtenhagen <maurice@blockchain.com>
2018-09-11 19:01:20 -0400

    Revert "fix(KYC): remove back button from application completed screen"
    This reverts commit 6c28010941f36d2beeed4a9b7a72538f550496a9.

--- Blockchain/KYC/KYCApplicationCompleteController.swift
+++ Blockchain/KYC/KYCApplicationCompleteController.swift
@@ -27,7 +27,6 @@ final class KYCApplicationCompleteController: KYCBaseViewController, Progressabl
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.navigationItem.leftBarButtonItem = UIBarButtonItem()
         setupProgressView()
     }
 
