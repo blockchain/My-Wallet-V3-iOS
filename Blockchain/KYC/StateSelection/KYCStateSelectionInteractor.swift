616c2e4f027d9a08f48097d0e97f069e48f298cf
Chris Arriola <arriola@blockchain.com>
2018-10-09 14:59:07 -0700

    feat(KYC): show state selection screen if selected country has states (IOS-1390)

---
+++ Blockchain/KYC/StateSelection/KYCStateSelectionInteractor.swift
@@ -0,0 +1,20 @@
+//
+//  KYCStateSelectionInteractor.swift
+//  Blockchain
+//
+//  Created by Chris Arriola on 10/9/18.
+//  Copyright © 2018 Blockchain Luxembourg S.A. All rights reserved.
+//
+
+import RxSwift
+
+class KYCStateSelectionInteractor {
+
+    func fetchState(for country: KYCCountry) -> Single<[KYCState]> {
+        return KYCNetworkRequest.request(
+            get: .listOfStates,
+            pathComponents: country.urlPathComponentsForState,
+            type: [KYCState].self
+        )
+    }
+}
