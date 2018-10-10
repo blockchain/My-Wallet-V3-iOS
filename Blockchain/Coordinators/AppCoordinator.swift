2c4cdb5d5e6f120aa0cbcc59de3ea7dcb3e0bf58
Alex McGregor <amcgregor@blockchain.com>
2018-10-10 14:58:15 -0500

    feat(Lockbox): Resolving IOS-1251 - Now showing lockbox side menu item.

--- Blockchain/Coordinators/AppCoordinator.swift
+++ Blockchain/Coordinators/AppCoordinator.swift
@@ -222,6 +222,9 @@ extension AppCoordinator: SideMenuViewControllerDelegate {
             handleBuyBitcoin()
         case .exchange:
             handleExchange()
+        case .lockbox:
+            // TODO: Present `Lockbox` screen.
+            return
         }
     }
 
