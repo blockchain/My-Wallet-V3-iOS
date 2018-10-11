d0d6bf7661b3af580a72911f6d8efb95f18a1bcd
woudini <10579422+woudini@users.noreply.github.com>
2018-10-11 14:43:53 -0400

    fix(KYCInformationViewModel): hide button if accountStatus is expired or none

--- Blockchain/KYC/Information/KYCInformationViewModel.swift
+++ Blockchain/KYC/Information/KYCInformationViewModel.swift
@@ -111,7 +111,7 @@ extension KYCInformationViewConfig {
             isPrimaryButtonEnabled = true
         case .failed, .expired, .none:
             titleColor = UIColor.error
-            isPrimaryButtonEnabled = accountStatus != .failed
+            isPrimaryButtonEnabled = false
         case .pending:
             titleColor = UIColor.orange
             isPrimaryButtonEnabled = !UIApplication.shared.isRegisteredForRemoteNotifications
