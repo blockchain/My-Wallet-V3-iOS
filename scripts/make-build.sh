c211af0959cfae9395e336db90f4e9879db1751a
Maurice <maurice@achtenhagen.me>
2018-11-16 17:17:43 -0500

    feat(CI): IOS-1618 Automate uploading of builds to TestFlight (#115)
    * feat(CI): add archive and export job
    
    * feat(CI): update Fastlane config
    
    * chore(Project): add missing bundle icon entry
    
    * chore(Project): update code signing profiles
    
    * chore(project): update Podfile.lock
    
    * feat(CI): add Gymfile
    
    * feat(CI): temporarily allow branch to trigger build
    
    * feat(CI): trigger Match to update code signing
    
    * fix(CI): use SSH for repo cloning
    
    * feat(CI): disable slack message after running Match
    
    * feat(CI): archive in Fastlane job
    
    * feat(CI): use type appstore for code signing by default
    
    * feat(CI): read values from environment variables
    
    * feat(CI): move archive step back
    
    * chore(CI): clean up
    
    * chore(CI): use Xcode 10.1
    
    * fix(CI): move codesign step to archive job
    
    * chore(CI): rename fastlane lane
    
    * chore(CI): remove unused env variable
    
    * docs(CI): improve CI job docs
    
    * docs(CI): improve fastlane docs
    
    * chore(Project): enable Bitcode
    
    * chore(Project): store App Icon Set in project
    
    * chore(Project): use same bundle display name for all schemes
    
    * chore(Podfile): cleanup
    
    * chore(Project): update Podfile.lock
    
    * feat(CI): update CircleCI config
    
    * chore(Project): set CFBundleIconName
    
    * fix(CI): fix xcconfig file path
    
    * chore(Project): update Podfile.lock
    
    * chore(Project): enable agvtool
    
    * chore(Project): set current project version and update build number
    
    * chore(CI): remove unused code
    
    * chore(CI): trigger build for release-test branch
    
    * feat(CI): run tests and archive in parallel
    
    * chore(CI): do not archive on custom branch
    
    * docs(CI): update CI docs
    
    * feat(CI): add separate workflow for deployment
    
    * refactor(CI): rename tests workflow [ci skip]
    
    * docs(CI): add note about skipping build [ci skip]
    
    * chore(CI): rename workflows [ci skip]
    
    * feat(CI): enable build upload action
    
    * feat(CI): add build upload job
    
    * chore(CI): do not persist archive
    
    * refactor(CI): condense code
    
    * chore(CI): change workflow name
    
    * fix(CI): persist output directory to workspace before upload
    
    * feat(CI): install Pods in parallel
    
    * feat(CI): require manual approval prior to build upload
    
    * docs(CI): fixup docs
    
    * feat(CI): filter tags on archive job
    
    * feat(CI): add script to make builds

---
+++ scripts/make-build.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+#
+#  make-build.sh
+#  Blockchain
+#
+#  Created by Maurice A. on 11/12/18.
+#  Copyright © 2018 Blockchain Luxembourg S.A. All rights reserved.
+#
+#  Compatibility
+#  -------------
+#  ‣ This script only runs on macOS using Bash 3.0+
+#  ‣ Requires Xcode Command Line Tools.
+#
+
+set -eu
+set -o pipefail
+
+if [ -n "$(git status --untracked-files=no --porcelain)" ]; then
+  printf '\e[1;31m%-6s\e[m\n' "Making a new build requires that you have a clean git working directory. Please commit your changes or stash them to continue."
+  exit 1
+fi
+
+if ! [ -e "Blockchain.xcodeproj" ]; then
+    printf '\e[1;31m%-6s\e[m\n' "Unable to find the Xcode project file. Please ensure you are in the root directory of this project."
+    exit 1
+fi
+
+if ! [ -x "$(command -v agvtool)" ]; then
+  printf '\e[1;31m%-6s\e[m\n' "You are missing the Xcode Command Line Tools. To install them, please run: xcode-select --install."
+  exit 1
+fi
+
+printf "Copyright © 2018 Blockchain Luxembourg S.A. All rights reserved.\n"
+printf "You are about to make a new build. Please follow the instructions carefully.\n\n"
+printf '\e[1;34m%-6s\e[m\n\n' "\"With Great Power Comes Great Responsibility\" -Voltaire"
+
+read -p "‣ Enter the new value for the project version (e.g. 2.3.4), followed by [ENTER]: " project_version_number
+
+if ! [[ $project_version_number =~ ^[0-9]+\.[0-9]+\.[0-9]+ ]]; then
+  printf '\n\e[1;31m%-6s\e[m\n' "You have entered an invalid version number."
+  exit 1
+fi
+
+read -p "‣ Next, enter the new value for the project build (e.g. 5), followed by [ENTER]: " project_build_number
+
+if ! [[ $project_build_number =~ ^[0-9]+ ]]; then
+  printf '\n\e[1;31m%-6s\e[m\n' "You have entered an invalid build number."
+  exit 1
+fi
+
+git_tag="v${project_version_number}(${project_build_number})"
+
+if [ $(git tag -l "$git_tag") ]; then
+  printf '\n\e[1;31m%-6s\e[m\n' "The version you entered already exists!"
+  exit 1
+fi
+
+local_branch="maurice/IOS-1618/automate-build-upload"
+release_branch="release-test"
+user_branch=$(git branch | grep \* | cut -d ' ' -f2)
+build_number="${project_version_number}.${project_build_number}"
+printf "\nPlease review the information about your build below:\n"
+printf "Xcode project version to use (CFBundleShortVersionString): ${project_version_number}\n"
+printf "Xcode project build number to use (CFBundleVersion): ${build_number}\n"
+printf "Git tag to use: ${git_tag}\n\n"
+read -p "‣ Would you like to proceed? [y/N]: " answer
+if printf "$answer" | grep -iq "^n" ; then
+  printf '\e[1;31m%-6s\e[m' "Aborted the build process."
+  exit 6
+fi
+git checkout $release_branch > /dev/null 2>&1
+git pull origin $release_branch > /dev/null 2>&1
+git merge $local_branch > /dev/null 2>&1
+agvtool new-marketing-version $project_version_number > /dev/null 2>&1
+agvtool new-version -all $build_number > /dev/null 2>&1
+git add Blockchain.xcodeproj/project.pbxproj
+git add Blockchain/Blockchain-Info.plist
+git add BlockchainTests/Info.plist
+git commit -m "version bump: ${git_tag}" > /dev/null 2>&1
+git tag -s $git_tag -m "Release ${project_version_number}" > /dev/null 2>&1
+git push origin $git_tag > /dev/null 2>&1
+git push origin $release_branch > /dev/null 2>&1
+git-changelog -t $(git describe --abbrev=0) > /dev/null 2>&1
+read -p "‣ Would you like to copy the contents of Changelog.md to your clipboard? [y/N]: " answer
+if printf "$answer" | grep -iq "^y" ; then
+  cat Changelog.md | pbcopy
+fi
+rm Changelog.md
+git checkout $user_branch > /dev/null 2>&1
+printf '\n\e[1;32m%-6s\e[m\n' "Everything completed successfully 🎉"
