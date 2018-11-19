d776b41876513096d984f6306a54b5d7a5754d7f
woudini <10579422+woudini@users.noreply.github.com>
2018-11-19 11:46:06 -0500

    fix(Scripts): fix line breaks

--- scripts/make-build.sh
+++ scripts/make-build.sh
@@ -62,9 +62,9 @@ build_number="${project_version_number}.${project_build_number}"
 printf "\nPlease review the information about your build below:\n"
 printf "Xcode project version to use (CFBundleShortVersionString): ${project_version_number}\n"
 printf "Xcode project build number to use (CFBundleVersion): ${project_build_number}\n"
-printf "Git tag to use: ${git_tag}\n\n"
+printf "Git tag to use: ${git_tag}\n"
 printf "Development branch (will be merged into Release branch): ${local_branch}\n"
-printf "Release branch: ${release_branch}\n"
+printf "Release branch: ${release_branch}\n\n"
 read -p "‣ Would you like to proceed? [y/N]: " answer
 if printf "$answer" | grep -iq "^n" ; then
   printf '\e[1;31m%-6s\e[m' "Aborted the build process."
