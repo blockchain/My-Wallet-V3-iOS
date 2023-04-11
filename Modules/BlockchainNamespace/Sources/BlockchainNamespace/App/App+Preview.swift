65e6861f75f3dad0d8e18e4783f639b0e0d346de
paulo-bc <60097845+paulo-bc@users.noreply.github.com>
2023-04-11 12:20:55 +0100

    chore(namespace): add mock data to App.preview (#5320)
    ## Objective
    
    add basic mock data to App.preview
    
    ## Merge Checklist
    
    - [X] The PR uses a title supported by
    [.changelogrc](https://github.com/blockchain/My-Wallet-V3-iOS/blob/master/.changelogrc).
    - [ ] You have added sufficient documentation (descriptive comments), if
    applicable.

---
+++ Modules/BlockchainNamespace/Sources/BlockchainNamespace/App/App+Preview.swift
@@ -0,0 +1,68 @@
+// Copyright © Blockchain Luxembourg S.A. All rights reserved.
+
+import Combine
+import Extensions
+import Foundation
+import OptionalSubscripts
+
+extension App {
+
+    public static var preview: AppProtocol = debug()
+
+#if DEBUG
+    public static var test: App.Test { App.Test() }
+#endif
+
+    /// Creates a mocked AppProtocol instance.
+    public static func debug(
+        preferences: Preferences = Mock.Preferences(),
+        remoteConfiguration: some RemoteConfiguration_p = Mock.RemoteConfiguration(),
+        session: URLSessionProtocol = URLSession.test,
+        scheduler: AnySchedulerOf<DispatchQueue> = DispatchQueue.test.eraseToAnyScheduler()
+    ) -> AppProtocol {
+        App(
+            state: with(Session.State([:], preferences: preferences)) { state in
+                state.data.keychain = (
+                    user: Mock.Keychain(queryProvider: state.data.keychainAccount.user),
+                    shared: Mock.Keychain(queryProvider: state.data.keychainAccount.shared)
+                )
+            },
+            remoteConfiguration: Session.RemoteConfiguration(
+                remote: remoteConfiguration,
+                session: session,
+                preferences: preferences,
+                scheduler: scheduler
+            )
+        )
+    }
+}
+
+extension AppProtocol {
+    public func withPreviewData(
+        fiatCurrency: String = "USD"
+    ) -> AppProtocol {
+        setup { app in
+            app.state.set(blockchain.user.id, to: "User")
+            app.state.set(blockchain.user.currency.preferred.fiat.trading.currency, to: fiatCurrency)
+            app.state.set(blockchain.api.nabu.gateway.price.crypto.fiat.id, to: fiatCurrency)
+            try await app.register(
+                napi: blockchain.api.nabu.gateway.price,
+                domain: blockchain.api.nabu.gateway.price.crypto.fiat,
+                repository: { tag in
+                    do {
+                        return try .just(
+                            [
+                                "currency": tag.indices[blockchain.api.nabu.gateway.price.crypto.id].decode(String.self),
+                                "quote": [
+                                    "value": ["amount": "34511", "currency": tag.indices[blockchain.api.nabu.gateway.price.crypto.fiat.id].decode(String.self)] as [String : Any]
+                                ]
+                            ]
+                        )
+                    } catch {
+                        return .just(.empty)
+                    }
+                }
+            )
+        }
+    }
+}
