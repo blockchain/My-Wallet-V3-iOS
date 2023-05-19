a9284d6d6c29e17e85ba28e81a9cf3133020b8a5
audrea-bc <102520557+audrea-bc@users.noreply.github.com>
2023-05-19 15:53:38 +0200

    fix(release) - Rebase on current dev  (#5437)
    ## Objective
    
    Rebasing 202305.1.0 on dev
    
    ---------
    
    Signed-off-by: dchatzieleftheriou-bc <dchatzieleftheriou@blockchain.com>
    Co-authored-by: dchatzieleftheriou-bc <71259279+dchatzieleftheriou-bc@users.noreply.github.com>
    Co-authored-by: Oliver Atkinson <88668746+oliveratkinson-bc@users.noreply.github.com>
    Co-authored-by: Clément Rucheton <90903183+crucheton-bc@users.noreply.github.com>

--- Modules/FeatureWalletConnect/Sources/FeatureWalletConnectDomain/Data/Services/WalletConnectCryptoProvider.swift
+++ Modules/FeatureWalletConnect/Sources/FeatureWalletConnectDomain/Data/Services/WalletConnectCryptoProvider.swift
@@ -4,31 +4,11 @@ import CryptoSwift
 import EthereumKit
 import Foundation
 import ToolKit
-import Web3
 import Web3Wallet
 
 final class WalletConnectCryptoProvider: CryptoProvider {
     func recoverPubKey(signature: EthereumSignature, message: Data) throws -> Data {
         try EthereumKit.recoverPubKey(from: signature.serialized, message: message)
-    }
-
-    func keccak256(_ data: Data) -> Data {
-        let digest = SHA3(variant: .keccak256)
-        let hash = digest.calculate(for: [UInt8](data))
-        return Data(hash)
-    }
-}
-
-struct DefaultCryptoProvider: CryptoProvider {
-
-    func recoverPubKey(signature: EthereumSignature, message: Data) throws -> Data {
-        let publicKey = try EthereumPublicKey(
-            message: message.bytes,
-            v: EthereumQuantity(quantity: BigUInt(signature.v)),
-            r: EthereumQuantity(signature.r),
-            s: EthereumQuantity(signature.s)
-        )
-        return Data(publicKey.rawPublicKey)
     }
 
     func keccak256(_ data: Data) -> Data {
