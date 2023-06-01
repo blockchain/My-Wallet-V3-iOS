f247979f85ab3dde73a58e69f1d4862f35df4eff
audrea-bc <audrea@blockchain.com>
2023-06-01 16:40:50 +0200

    disable button if we don't have a source and target

--- Modules/FeatureTransaction/Sources/FeatureTransactionUI/Swap Enter Amount/SwapEnterAmount.swift
+++ Modules/FeatureTransaction/Sources/FeatureTransactionUI/Swap Enter Amount/SwapEnterAmount.swift
@@ -68,7 +68,10 @@ public struct SwapEnterAmount: ReducerProtocol {
         }
 
         var previewButtonDisabled: Bool {
-            finalSelectedMoneyValue == nil || finalSelectedMoneyValue?.isZero == true
+            guard sourceInformation != nil, targetInformation != nil, let finalSelectedMoneyValue else {
+                return true
+            }
+            return finalSelectedMoneyValue.isZero
         }
 
         var transactionDetails: (forbidden: Bool, ctaLabel: String) {
