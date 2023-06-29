bfdd92795ac47969ff509a9eacb0a8a9f9d516a0
audrea-bc <audrea@blockchain.com>
2023-06-29 14:02:28 +0200

    fixes

--- Modules/FeatureDex/Sources/UI/Network Picker/NetworkPicker.swift
+++ Modules/FeatureDex/Sources/UI/Network Picker/NetworkPicker.swift
@@ -42,13 +42,18 @@ public struct NetworkPicker: ReducerProtocol {
                 return .none
 
             case .onNetworkSelected(let network):
+                state.currentNetwork = network.networkConfig.networkTicker
                 return .run { _ in
-                    try await app.set(blockchain.ux.currency.exchange.dex.network.picker.selected.network.ticker.then.close, to: true)
+                    try await app.set(blockchain.ux.currency.exchange.dex.network.picker.selected.network.ticker.entry.paragraph.row.tap.then.close, to: true)
+                    app.post(event: blockchain.ux.currency.exchange.dex.network.picker.selected.network.ticker.entry.paragraph.row.tap)
                     try await app.set(blockchain.ux.currency.exchange.dex.network.picker.selected.network.ticker,
                                       to: network.networkConfig.networkTicker)
                 }
             case .onDismiss:
-                return .none
+                return .run { _ in
+                    try await app.set(blockchain.ux.currency.exchange.dex.network.picker.selected.network.ticker.article.plain.navigation.bar.button.close.tap.then.close, to: true)
+                    app.post(event: blockchain.ux.currency.exchange.dex.network.picker.selected.network.ticker.article.plain.navigation.bar.button.close.tap)
+                }
             }
         }
     }
