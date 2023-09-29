79d82973ad5e19b2aa6c25c5fa400ffc0ee34567
Oliver Atkinson <88668746+oliveratkinson-bc@users.noreply.github.com>
2023-09-29 14:54:46 +0100

    fix(Crash): Update Session+RemoteConfiguration.swift (#6032)
    ## Description
    
    If remote config fails, don't overflow ... needs testing
    
    
    https://console.firebase.google.com/u/1/project/fir-production/crashlytics/app/ios:com.rainydayapps.Blockchain/issues/c1786f54ae6abf4da1934fd65c27f90a?utm_campaign=extensions&utm_medium=SLACK&utm_source=VELOCITY_ISSUE&time=last-seven-days&versions=202310.1.0%20(15117);202310.1.0%20(15116);202310.1.0%20(15115);202310.1.0%20(15113);202310.1.0%20(15109);202310.1.0%20(15108)&types=crash&sessionEventKey=31c2037e81574ac18e981de70d1f7cd8_1862680465079369226
    
    ## Merge Checklist
    
    - [x] The PR uses a title supported by
    [.changelogrc](https://github.com/blockchain/My-Wallet-V3-iOS/blob/master/.changelogrc).

--- Modules/BlockchainNamespace/Sources/BlockchainNamespace/Session/Session+RemoteConfiguration.swift
+++ Modules/BlockchainNamespace/Sources/BlockchainNamespace/Session/Session+RemoteConfiguration.swift
@@ -464,12 +464,13 @@ private actor ExponentialBackoff {
     }
 
     func next() async throws {
-        n += 1
+        n = min(n + 1, 15)
+        let time = unit * TimeInterval.random(
+            in: 1...pow(2, n.d),
+            using: &rng
+        )
         try await Task.sleep(
-            nanoseconds: UInt64(TimeInterval.random(
-                in: unit...unit * pow(2, TimeInterval(n - 1)),
-                using: &rng
-            ) * 1_000_000)
+            nanoseconds: min(time * NSEC_PER_SEC.d, UInt64.max.d).u64
         )
     }
 }
