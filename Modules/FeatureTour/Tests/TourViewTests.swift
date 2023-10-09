f0dc5bcf8b6d0158e65f4a1d6cb965d0b6b8c2e0
Paulo <paulo@blockchain.com>
2023-10-09 10:24:00 +0100

    park

--- Modules/FeatureTour/Tests/TourViewTests.swift
+++ Modules/FeatureTour/Tests/TourViewTests.swift
@@ -16,173 +16,153 @@ final class TourViewTests: XCTestCase {
         isRecording = false
     }
 
-    func x_testTourView_manualLogin_disabled() {
-        let view = OnboardingCarouselView(
-            environment: TourEnvironment(
-                createAccountAction: {},
-                restoreAction: {},
-                logInAction: {},
-                manualLoginAction: {}
-            ),
-            manualLoginEnabled: false
-        )
-        assertSnapshot(
-            matching: view,
-            as: .image(
-                perceptualPrecision: 0.98,
-                layout: .device(config: .iPhone8Plus),
-                traits: UITraitCollection(userInterfaceStyle: .light)
-            )
-        )
-
-        let brokerageView = OnboardingCarouselView.Carousel.brokerage.makeView()
-        assertSnapshot(
-            matching: brokerageView,
-            as: .image(
-                perceptualPrecision: 0.98,
-                layout: .device(config: .iPhone8Plus),
-                traits: UITraitCollection(userInterfaceStyle: .light)
-            )
-        )
-
-        let earnView = OnboardingCarouselView.Carousel.earn.makeView()
-        assertSnapshot(
-            matching: earnView,
-            as: .image(
-                perceptualPrecision: 0.98,
-                layout: .device(config: .iPhone8Plus),
-                traits: UITraitCollection(userInterfaceStyle: .light)
-            )
-        )
-
-        let keysView = OnboardingCarouselView.Carousel.keys.makeView()
-        assertSnapshot(
-            matching: keysView,
-            as: .image(
-                perceptualPrecision: 0.98,
-                layout: .device(config: .iPhone8Plus),
-                traits: UITraitCollection(userInterfaceStyle: .light)
-            )
-        )
-
-        let items = [
-            Price(currency: .bitcoin, value: .loaded(next: "$55,343.76"), deltaPercentage: .loaded(next: 7.88)),
-            Price(currency: .ethereum, value: .loaded(next: "$3,585.69"), deltaPercentage: .loaded(next: 1.82)),
-            Price(currency: .bitcoinCash, value: .loaded(next: "$618.05"), deltaPercentage: .loaded(next: -3.46)),
-            Price(currency: .stellar, value: .loaded(next: "$0.36"), deltaPercentage: .loaded(next: 12.50))
-        ]
-        var tourState = TourState()
-        tourState.items = IdentifiedArray(uniqueElements: items)
-        let mockTourReducer: Reducer<TourState, TourAction, TourEnvironment> = Reducer { _, _, _ in
-            .none
-        }
-        let tourStore = Store(
-            initialState: tourState,
-            reducer: mockTourReducer,
-            environment: TourEnvironment(
-                createAccountAction: {},
-                restoreAction: {},
-                logInAction: {},
-                manualLoginAction: {}
-            )
-        )
-        let livePricesView = LivePricesView(
-            store: tourStore,
-            list: LivePricesList(store: tourStore)
-        )
-        assertSnapshot(
-            matching: livePricesView,
-            as: .image(
-                perceptualPrecision: 0.98,
-                layout: .device(config: .iPhone8Plus),
-                traits: UITraitCollection(userInterfaceStyle: .light)
-            )
-        )
-    }
-
-    func x_testTourView_manualLogin_enabled() {
-        let view = OnboardingCarouselView(
-            environment: TourEnvironment(
-                createAccountAction: {},
-                restoreAction: {},
-                logInAction: {},
-                manualLoginAction: {}
-            ),
-            manualLoginEnabled: true
-        )
-        assertSnapshot(
-            matching: view,
-            as: .image(
-                perceptualPrecision: 0.98,
-                layout: .device(config: .iPhone8Plus),
-                traits: UITraitCollection(userInterfaceStyle: .light)
-            )
-        )
-
-        let brokerageView = OnboardingCarouselView.Carousel.brokerage.makeView()
-        assertSnapshot(
-            matching: brokerageView,
-            as: .image(
-                perceptualPrecision: 0.98,
-                layout: .device(config: .iPhone8Plus),
-                traits: UITraitCollection(userInterfaceStyle: .light)
-            )
-        )
-
-        let earnView = OnboardingCarouselView.Carousel.earn.makeView()
-        assertSnapshot(
-            matching: earnView,
-            as: .image(
-                perceptualPrecision: 0.98,
-                layout: .device(config: .iPhone8Plus),
-                traits: UITraitCollection(userInterfaceStyle: .light)
-            )
-        )
-
-        let keysView = OnboardingCarouselView.Carousel.keys.makeView()
-        assertSnapshot(
-            matching: keysView,
-            as: .image(
-                perceptualPrecision: 0.98,
-                layout: .device(config: .iPhone8Plus),
-                traits: UITraitCollection(userInterfaceStyle: .light)
-            )
-        )
-
-        let items = [
-            Price(currency: .bitcoin, value: .loaded(next: "$55,343.76"), deltaPercentage: .loaded(next: 7.88)),
-            Price(currency: .ethereum, value: .loaded(next: "$3,585.69"), deltaPercentage: .loaded(next: 1.82)),
-            Price(currency: .bitcoinCash, value: .loaded(next: "$618.05"), deltaPercentage: .loaded(next: -3.46)),
-            Price(currency: .stellar, value: .loaded(next: "$0.36"), deltaPercentage: .loaded(next: 12.50))
-        ]
-        var tourState = TourState()
-        tourState.items = IdentifiedArray(uniqueElements: items)
-        let mockTourReducer: Reducer<TourState, TourAction, TourEnvironment> = Reducer { _, _, _ in
-            .none
-        }
-        let tourStore = Store(
-            initialState: tourState,
-            reducer: mockTourReducer,
-            environment: TourEnvironment(
-                createAccountAction: {},
-                restoreAction: {},
-                logInAction: {},
-                manualLoginAction: {}
-            )
-        )
-        let livePricesView = LivePricesView(
-            store: tourStore,
-            list: LivePricesList(store: tourStore)
-        )
-        assertSnapshot(
-            matching: livePricesView,
-            as: .image(
-                perceptualPrecision: 0.98,
-                layout: .device(config: .iPhone8Plus),
-                traits: UITraitCollection(userInterfaceStyle: .light)
-            )
-        )
-    }
+//    func testTourView_manualLogin_disabled() {
+//        let view = OnboardingCarouselView(
+//            store: Store(
+//                initialState: TourState(),
+//                reducer: NoOpReducer()
+//            ),
+//            manualLoginEnabled: false
+//        )
+//        assertSnapshot(
+//            matching: view,
+//            as: .image(
+//                perceptualPrecision: 0.98,
+//                layout: .device(config: .iPhone8Plus),
+//                traits: UITraitCollection(userInterfaceStyle: .light)
+//            )
+//        )
+//
+//        let brokerageView = OnboardingCarouselView.Carousel.brokerage.makeView()
+//        assertSnapshot(
+//            matching: brokerageView,
+//            as: .image(
+//                perceptualPrecision: 0.98,
+//                layout: .device(config: .iPhone8Plus),
+//                traits: UITraitCollection(userInterfaceStyle: .light)
+//            )
+//        )
+//
+//        let earnView = OnboardingCarouselView.Carousel.earn.makeView()
+//        assertSnapshot(
+//            matching: earnView,
+//            as: .image(
+//                perceptualPrecision: 0.98,
+//                layout: .device(config: .iPhone8Plus),
+//                traits: UITraitCollection(userInterfaceStyle: .light)
+//            )
+//        )
+//
+//        let keysView = OnboardingCarouselView.Carousel.keys.makeView()
+//        assertSnapshot(
+//            matching: keysView,
+//            as: .image(
+//                perceptualPrecision: 0.98,
+//                layout: .device(config: .iPhone8Plus),
+//                traits: UITraitCollection(userInterfaceStyle: .light)
+//            )
+//        )
+//
+//        let items = [
+//            Price(currency: .bitcoin, value: .loaded(next: "$55,343.76"), deltaPercentage: .loaded(next: 7.88)),
+//            Price(currency: .ethereum, value: .loaded(next: "$3,585.69"), deltaPercentage: .loaded(next: 1.82)),
+//            Price(currency: .bitcoinCash, value: .loaded(next: "$618.05"), deltaPercentage: .loaded(next: -3.46)),
+//            Price(currency: .stellar, value: .loaded(next: "$0.36"), deltaPercentage: .loaded(next: 12.50))
+//        ]
+//        var tourState = TourState()
+//        tourState.items = IdentifiedArray(uniqueElements: items)
+//
+//        let tourStore = Store(
+//            initialState: tourState,
+//            reducer: NoOpReducer()
+//        )
+//        let livePricesView = LivePricesView(
+//            store: tourStore,
+//            list: LivePricesList(store: tourStore)
+//        )
+//        assertSnapshot(
+//            matching: livePricesView,
+//            as: .image(
+//                perceptualPrecision: 0.98,
+//                layout: .device(config: .iPhone8Plus),
+//                traits: UITraitCollection(userInterfaceStyle: .light)
+//            )
+//        )
+//    }
+//
+//    func testTourView_manualLogin_enabled() {
+//        let view = OnboardingCarouselView(
+//            store: Store(
+//                initialState: TourState(),
+//                reducer: NoOpReducer()
+//            ),
+//            manualLoginEnabled: true
+//        )
+//        assertSnapshot(
+//            matching: view,
+//            as: .image(
+//                perceptualPrecision: 0.98,
+//                layout: .device(config: .iPhone8Plus),
+//                traits: UITraitCollection(userInterfaceStyle: .light)
+//            )
+//        )
+//
+//        let brokerageView = OnboardingCarouselView.Carousel.brokerage.makeView()
+//        assertSnapshot(
+//            matching: brokerageView,
+//            as: .image(
+//                perceptualPrecision: 0.98,
+//                layout: .device(config: .iPhone8Plus),
+//                traits: UITraitCollection(userInterfaceStyle: .light)
+//            )
+//        )
+//
+//        let earnView = OnboardingCarouselView.Carousel.earn.makeView()
+//        assertSnapshot(
+//            matching: earnView,
+//            as: .image(
+//                perceptualPrecision: 0.98,
+//                layout: .device(config: .iPhone8Plus),
+//                traits: UITraitCollection(userInterfaceStyle: .light)
+//            )
+//        )
+//
+//        let keysView = OnboardingCarouselView.Carousel.keys.makeView()
+//        assertSnapshot(
+//            matching: keysView,
+//            as: .image(
+//                perceptualPrecision: 0.98,
+//                layout: .device(config: .iPhone8Plus),
+//                traits: UITraitCollection(userInterfaceStyle: .light)
+//            )
+//        )
+//
+//        let items = [
+//            Price(currency: .bitcoin, value: .loaded(next: "$55,343.76"), deltaPercentage: .loaded(next: 7.88)),
+//            Price(currency: .ethereum, value: .loaded(next: "$3,585.69"), deltaPercentage: .loaded(next: 1.82)),
+//            Price(currency: .bitcoinCash, value: .loaded(next: "$618.05"), deltaPercentage: .loaded(next: -3.46)),
+//            Price(currency: .stellar, value: .loaded(next: "$0.36"), deltaPercentage: .loaded(next: 12.50))
+//        ]
+//        var tourState = TourState()
+//        tourState.items = IdentifiedArray(uniqueElements: items)
+//
+//        let tourStore = Store(
+//            initialState: tourState,
+//            reducer: NoOpReducer()
+//        )
+//        let livePricesView = LivePricesView(
+//            store: tourStore,
+//            list: LivePricesList(store: tourStore)
+//        )
+//        assertSnapshot(
+//            matching: livePricesView,
+//            as: .image(
+//                perceptualPrecision: 0.98,
+//                layout: .device(config: .iPhone8Plus),
+//                traits: UITraitCollection(userInterfaceStyle: .light)
+//            )
+//        )
+//    }
 }
 
 /// This is needed in order to resolve the dependencies
