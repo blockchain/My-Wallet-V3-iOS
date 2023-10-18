79d84f7e5426624ad43bbcb8eed70d71a0a17ac6
audrea-bc <102520557+audrea-bc@users.noreply.github.com>
2023-10-18 15:57:57 +0200

    fix(growth): GROWUX-0000 App mode default fix 🍒 (#6109)
    ## Objective
    
    🍒 to dev
    
    Added a new decision to default the app to trading when possible. This
    will allow us to redirect the user to the appropriate mode taking the
    products endpoint into account.
    
    
    Future simplifications: 
    
    - Remove the app mode defaulting feature flag. It's very old and it was
    introduced to redirect high risk country users to the PKW mode.

--- BlockchainTests/AppModeDecisionTests.swift
+++ BlockchainTests/AppModeDecisionTests.swift
@@ -9,7 +9,8 @@ class AppModeDecisionTests: XCTestCase {
             useTradingAccount: ProductValue(id: .useTradingAccount, enabled: true, defaultProduct: true),
             useExternalTradingAccount: ProductValue(id: .useExternalTradingAccount, enabled: true, defaultProduct: true),
             isDefaultingEnabled: true,
-            hasBeenDefaultedAlready: false
+            hasBeenDefaultedToDefiAlready: false,
+            hasBeenDefaultedToTradingAlready: false
         )
         XCTAssertFalse(decision.shouldDefaultToDeFi())
     }
@@ -19,7 +20,8 @@ class AppModeDecisionTests: XCTestCase {
             useTradingAccount: ProductValue(id: .useTradingAccount, enabled: true, defaultProduct: true),
             useExternalTradingAccount: ProductValue(id: .useExternalTradingAccount, enabled: false, defaultProduct: false),
             isDefaultingEnabled: true,
-            hasBeenDefaultedAlready: false
+            hasBeenDefaultedToDefiAlready: false,
+            hasBeenDefaultedToTradingAlready: false
         )
         XCTAssertFalse(decision.shouldDefaultToDeFi())
     }
@@ -29,7 +31,8 @@ class AppModeDecisionTests: XCTestCase {
             useTradingAccount: ProductValue(id: .useTradingAccount, enabled: false, defaultProduct: false),
             useExternalTradingAccount: ProductValue(id: .useExternalTradingAccount, enabled: true, defaultProduct: true),
             isDefaultingEnabled: true,
-            hasBeenDefaultedAlready: false
+            hasBeenDefaultedToDefiAlready: false,
+            hasBeenDefaultedToTradingAlready: false
         )
         XCTAssertFalse(decision.shouldDefaultToDeFi())
     }
@@ -39,8 +42,54 @@ class AppModeDecisionTests: XCTestCase {
             useTradingAccount: ProductValue(id: .useTradingAccount, enabled: false, defaultProduct: false),
             useExternalTradingAccount: ProductValue(id: .useExternalTradingAccount, enabled: false, defaultProduct: false),
             isDefaultingEnabled: true,
-            hasBeenDefaultedAlready: false
+            hasBeenDefaultedToDefiAlready: false,
+            hasBeenDefaultedToTradingAlready: false
         )
         XCTAssertTrue(decision.shouldDefaultToDeFi())
+    }
+
+    func testTradingDisabledExternalEnabledShouldDefaultToTrading() {
+        let decision = AppModeDecision(
+            useTradingAccount: ProductValue(id: .useTradingAccount, enabled: false, defaultProduct: false),
+            useExternalTradingAccount: ProductValue(id: .useExternalTradingAccount, enabled: true, defaultProduct: true),
+            isDefaultingEnabled: true,
+            hasBeenDefaultedToDefiAlready: false,
+            hasBeenDefaultedToTradingAlready: false
+        )
+        XCTAssertTrue(decision.shouldDefaultToTrading())
+    }
+
+    func testTradingEnabledExternalDisabledShouldDefaultToTrading() {
+        let decision = AppModeDecision(
+            useTradingAccount: ProductValue(id: .useTradingAccount, enabled: true, defaultProduct: true),
+            useExternalTradingAccount: ProductValue(id: .useExternalTradingAccount, enabled: false, defaultProduct: false),
+            isDefaultingEnabled: true,
+            hasBeenDefaultedToDefiAlready: false,
+            hasBeenDefaultedToTradingAlready: false
+        )
+        XCTAssertTrue(decision.shouldDefaultToTrading())
+    }
+
+
+    func testTradingDisabledExternalDisabledShouldNotDefaultToTrading() {
+        let decision = AppModeDecision(
+            useTradingAccount: ProductValue(id: .useTradingAccount, enabled: false, defaultProduct: false),
+            useExternalTradingAccount: ProductValue(id: .useExternalTradingAccount, enabled: false, defaultProduct: false), 
+            isDefaultingEnabled: true,
+            hasBeenDefaultedToDefiAlready: false,
+            hasBeenDefaultedToTradingAlready: false
+        )
+        XCTAssertFalse(decision.shouldDefaultToTrading())
+    }
+
+    func testTradingEnableddExternalDisabledAlreadyDefaultedShouldNotDefaultToTrading() {
+        let decision = AppModeDecision(
+            useTradingAccount: ProductValue(id: .useTradingAccount, enabled: true, defaultProduct: false),
+            useExternalTradingAccount: ProductValue(id: .useExternalTradingAccount, enabled: false, defaultProduct: false),
+            isDefaultingEnabled: true,
+            hasBeenDefaultedToDefiAlready: false,
+            hasBeenDefaultedToTradingAlready: true
+        )
+        XCTAssertFalse(decision.shouldDefaultToTrading())
     }
 }
